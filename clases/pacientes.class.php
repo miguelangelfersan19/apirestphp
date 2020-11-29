87a4d8668dcf47cc0f0e6c7812c1d097947f22f9
Miguel Angel Fernandez Sanjuanelo <miguelangelfersan19@gmail.com>
2020-11-29 14:21:20 -0500

    metodo delete update listos falta de token para los usuarios

--- clases/pacientes.class.php
+++ clases/pacientes.class.php
@@ -13,6 +13,7 @@ class pacientes extends conexion{
     private $genero  = "";
     private $telefono  = "";
     private $fechaNacimiento  = "0000-00-00";
+    private $correo = ""; 
 
 
 
@@ -39,14 +40,132 @@ class pacientes extends conexion{
         $datos = json_decode($json, true);
         if(!isset($datos['nombre']) || !isset($datos['dni']) || !isset($datos['correo'])){
             return $_respuestas->error_400();
+        }else {
+            $this->nombre = $datos['nombre'];
+            $this->dni = $datos['dni'];
+            $this->correo = $datos['correo'];
+            //validacion de datos solicitados
+            if(isset($datos['telefono'])){$this->telfono = $datos['telefono'];}
+            if(isset($datos['direccion'])){$this->direccion = $datos['direccion'];}
+            if(isset($datos['codigoPostal'])){$this->codigoPostal = $datos['codigoPostal'];}
+            if(isset($datos['genero'])){$this->genero = $datos['genero'];}
+            if(isset($datos['fechaNacimiento'])){$this->fechaNacimiento = $datos['fechaNacimiento'];}
+            $resp= $this->insertarPaciente();
+
+            if($resp){
+                $respuesta = $_respuestas->response;
+                $respuesta["result"] = array(
+                    "pacienteId" => $resp
+                );
+                return $respuesta;
+            }else{
+                return $_respuestas->error_500();
+            }
         }
+
+    }
+
+
+    private function insertarPaciente(){
+        $query = "INSERT INTO " . $this->table . " (DNI,Nombre,Direccion,CodigoPostal,Telefono,Genero,FechaNacimiento,Correo)
+        values
+        ('" . $this->dni . "','" . $this->nombre . "','" . $this->direccion ."','" . $this->codigoPostal . "','"  . $this->telefono . "','" . $this->genero . "','" . $this->fechaNacimiento . "','" . $this->correo . "')"; 
+        $resp = parent::nomQueryId($query);
+        print_r( $query);
+    }
+
+    public function put($json){
+        $_respuestas = new respuestas;
+        $datos = json_decode($json, true);
+        if(!isset($datos['pacienteId']) ){
+            return $_respuestas->error_400();
+        }else {
+            $this->pacienteid = $datos['pacienteId'];
+            if(isset($datos['nombre'])){$this->nombre= $datos['nombre'];}
+            if(isset($datos['dni'])){$this->dni = $datos['dni'];}
+            if(isset($datos['correo'])){$this->correo = $datos['correo'];}
+            if(isset($datos['telefono'])){$this->telfono = $datos['telefono'];}
+            if(isset($datos['direccion'])){$this->direccion = $datos['direccion'];}
+            if(isset($datos['codigoPostal'])){$this->codigoPostal = $datos['codigoPostal'];}
+            if(isset($datos['genero'])){$this->genero = $datos['genero'];}
+            if(isset($datos['fechaNacimiento'])){$this->fechaNacimiento = $datos['fechaNacimiento'];}
+            $resp= $this->modificarPaciente();
+
+            if($resp){
+                $respuesta = $_respuestas->response;
+                $respuesta["result"] = array(
+                    "pacienteId" => $resp
+                );
+                return $respuesta;
+            }else{
+                return $_respuestas->error_500();
+            }
+        }
+
+        
+    }
+
+
+    private function modificarPaciente(){
+                 $query = "UPDATE " . $this->table . " SET Nombre ='" . $this->nombre .
+          "',Direccion = '" . $this->direccion . "', DNI = '" . $this->dni .
+           "', CodigoPostal = '" . $this->codigoPostal . "', Telefono = '" . $this->telefono .
+            "', Genero = '" . $this->genero . "', FechaNacimiento = '" . $this->fechaNacimiento .
+             "', Correo = '" . $this->correo .
+          "' WHERE PacienteId = '" . $this->pacienteid . "'"; 
+
+        $resp = parent::nomQuery($query);
+        if ($resp >=1){
+            return $resp;
+
+        }else{
+            return 0;
+        }
+        
+    }
+
+    public function delete ($json){
+
+        $_respuestas = new respuestas;
+        $datos = json_decode($json, true);
+        if(!isset($datos['pacienteId']) ){
+            return $_respuestas->error_400();
+        }else {
+            $this->pacienteid = $datos['pacienteId'];
+            
+            $resp= $this->eliminarPaciente();
+
+            if($resp){
+                $respuesta = $_respuestas->response;
+                $respuesta["result"] = array(
+                    "pacienteId" => $resp
+                );
+                return $respuesta;
+            }else{
+                return $_respuestas->error_500();
+            }
+        }
+
+
+
+    }
+
+    private function eliminarPaciente(){
+        $query = "DELETE FROM " . $this->table . " WHERE PacienteId= '" . $this->pacienteid . "'";
+        $resp = parent::nomQuery($query);
+        if ($resp >=1) {
+            return $resp;   
+        }else{
+            return 0;
+        }
+
     }
 
 
 
 
-}
 
+}
 
 
 ?>
--- pacientes.php
+++ pacientes.php
@@ -27,14 +27,54 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
 
      } else if ($_SERVER['REQUEST_METHOD'] == "POST"){
       // se reciben los datos enviados
-      $postBody = file_get_contents("php://input"); // se envian los datos al manejador
-      $resp = $_pacientes->post($postBody);
-      print_r($resp);
+         $postBody = file_get_contents("php://input"); // se envian los datos al manejador
+         $resp = $_pacientes->post($postBody);
+         
+
+          header('Content-Type: application/json');
+         if(isset($datosArray["result"]["error_id"])){
+            $responseCode = $datosArray["result"]["error_id"];
+            http_response_code($responseCode);
+          }else{
+           http_response_code(200);
+          }
+           echo json_encode($datosArray);
+
+
 
      }else if ($_SERVER['REQUEST_METHOD']== "PUT"){
-     echo "put";
+    
+        $postBody = file_get_contents("php://input");
+        $datosArray = $_pacientes->put($postBody);
+        // se devuelve la respuesta
+          header('Content-Type: application/json');
+         if(isset($datosArray["result"]["error_id"])){
+           $responseCode = $datosArray["result"]["error_id"];
+           http_response_code($responseCode);
+           }else{
+            http_response_code(200);
+         }
+         echo json_encode($datosArray);
+
+
+
      }else if ($_SERVER['REQUEST_METHOD']== "DELETE"){
-     echo "dELETE";
+     
+
+        $postBody = file_get_contents("php://input");// recibimos los datos y la guardamos en la variable postBody
+        $datosArray = $_pacientes->delete($postBody);
+        // se devuelve la respuesta
+        
+          header('Content-Type: application/json');
+         if(isset($datosArray["result"]["error_id"])){
+           $responseCode = $datosArray["result"]["error_id"];
+           http_response_code($responseCode);
+           }else{
+            http_response_code(200);
+         }
+         echo json_encode($datosArray); 
+
+
      }else{
      header('content-Type application/json');
      $datosArray = $_respuestas->error_405;
